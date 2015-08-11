dbaaa30273cdebf82f57e928042dcf29c3345314
Christopher Brown <io@henrian.com>
2015-08-11 10:38:09 -0500

    Type declarations

---
+++ type_declarations/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
+
+/// <reference path="../node_modules/loge/loge.d.ts" />
