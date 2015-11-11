284ebba5a41d18fb2dc6c2979da4cbe0f6414aa4
Christopher Brown <io@henrian.com>
2015-11-11 10:01:57 -0600

    Remove local type_declarations

--- type_declarations/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
-
-/// <reference path="../node_modules/loge/loge.d.ts" />
